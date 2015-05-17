1c76af349ad93e0b375a383edd542f1679d5315a
kaspernj <k@spernj.org>
2015-05-17 14:05:22 +0200

    Bumped to 0.0.8. Added test for dir in translation inspector

--- spec/lib/erb_inspector/translation_inspector_spec.rb
+++ spec/lib/erb_inspector/translation_inspector_spec.rb
@@ -12,6 +12,14 @@ describe AwesomeTranslations::ErbInspector::TranslationInspector do
   let(:user_index_inspector) { files.select { |file_inspector| file_inspector.file_path == "app/views/users/index.html.haml" }.first }
   let(:user_index_translations) { user_index_inspector.translations.to_a }
   let(:hello_world_translation) { user_index_translations.select { |translation| translation.key == ".hello_world" }.first }
+  let(:user_was_created_translation) do
+    files
+      .select { |file_inspector| file_inspector.file_path == "app/controllers/users_controller.rb" }
+      .first
+      .translations
+      .select { |translation| translation.key == ".user_was_created" }
+      .first
+  end
 
   it "#full_key" do
     hello_world_translation.full_key.should eq "users.index.hello_world"
@@ -19,5 +27,9 @@ describe AwesomeTranslations::ErbInspector::TranslationInspector do
 
   it "#full_path" do
     hello_world_translation.full_path.should eq "#{Rails.root}/app/views/users/index.html.haml"
+  end
+
+  it '#dir' do
+    user_was_created_translation.dir.should end_with 'spec/dummy/config/locales/awesome_translations/app/controllers'
   end
 end
