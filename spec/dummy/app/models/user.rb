7e019d2a3167995d556e09a1c92ea189876c1732
kaspernj <k@spernj.org>
2015-05-20 11:49:50 +0200

    Support validates_confirmation_of validator

--- spec/dummy/app/models/user.rb
+++ spec/dummy/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   validates_presence_of :email
   validates_uniqueness_of :email
   validates :email, length: {in: 2..255}, format: {with: /\A.+@.+\Z/}
+  validates_confirmation_of :email
 
   has_many :roles, dependent: :destroy
 end
