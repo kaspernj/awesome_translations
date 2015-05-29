b1e07157fbb2765cd871a405f06e3af266106bb3
kaspernj <k@spernj.org>
2015-05-29 09:36:10 +0200

    Added translate functionality support to the class level.

--- lib/awesome_translations/translate_functionality.rb
+++ lib/awesome_translations/translate_functionality.rb
@@ -1,6 +1,14 @@
-# Include this class to get access to the t-method.
+# Include this module to get access to the t-method.
 module AwesomeTranslations::TranslateFunctionality
-  def t(key, args = {}, &blk)
-    AwesomeTranslations::GlobalTranslator.translate(key, translation_args: args, caller_number: 1, &blk)
+  # Implements the method on both class- and instance-level.
+  def self.included(base)
+    base.__send__(:include, Methods)
+    base.extend Methods
+  end
+
+  module Methods
+    def t(key, args = {}, &blk)
+      AwesomeTranslations::GlobalTranslator.translate(key, translation_args: args, caller_number: 1, &blk)
+    end
   end
 end
