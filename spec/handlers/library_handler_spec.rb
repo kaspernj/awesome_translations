109b2f649a66c7f6af6cfacf108b31b04aebd17e
kaspernj <k@spernj.org>
2015-08-01 12:51:55 +0200

    Implemented the controller_t method which skips the action in the key

--- spec/handlers/library_handler_spec.rb
+++ spec/handlers/library_handler_spec.rb
@@ -8,6 +8,8 @@ describe AwesomeTranslations::Handlers::LibraryHandler do
   let(:admin_translation) { group.translations.select { |translation| translation.key == "models.role.administrator" }.first }
   let(:application_helper_group) { handler.groups.select { |group| group.name == 'app/helpers/application_helper.rb' }.first }
   let(:hello_world_translation) { application_helper_group.translations.select { |translation| translation.key == 'helpers.application_helper.hello_world' }.first }
+  let(:users_controller_handler) { handler.groups.select { |group| group.name == 'app/controllers/users_controller.rb' }.first }
+  let(:update_saved_translation) { users_controller_handler.translations.select { |translation| translation.key == 'users.user_was_updated' }.first }
 
   it "finds translations made with the t method" do
     admin_translation.should_not eq nil
@@ -29,5 +31,11 @@ describe AwesomeTranslations::Handlers::LibraryHandler do
 
   it 'finds helpers translations using helper_t' do
     expect(hello_world_translation.key).to eq 'helpers.application_helper.hello_world'
+  end
+
+  it 'finds translations with the controller_t-method' do
+    expect(update_saved_translation).to_not eq nil
+    expect(update_saved_translation.key).to eq 'users.user_was_updated'
+    expect(update_saved_translation.dir).to end_with 'spec/dummy/config/locales/awesome_translations/app/controllers/users_controller'
   end
 end
